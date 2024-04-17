6c7dd740b09c09b4da213077744702628d9ea6cd
myngocvo <vtmn22070312@gmail.com>
2024-04-17 23:12:19 +0700

    Backend View Product Category [WB-83]

--- src/controllers/productTypeController.js
+++ src/controllers/productTypeController.js
@@ -1,50 +1,5 @@
 import productTypeService from "../services/productTypeService";
 
-let handleCreateNewProductType = async (req, res) => {
-  try {
-    let message = await productTypeService.createNewProductTypeService(
-      req.body
-    );
-    if (message.errCode === 0) return res.status(201).json(message);
-    else return res.status(400).json(message);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      errCode: -1,
-      message: "Error form the server!!!",
-    });
-  }
-};
-
-let handleDeleteProductType = async (req, res) => {
-  try {
-    let id = req.query.id;
-    let message = await productTypeService.deleteProductTypeService(id);
-    if (message.errCode === 0) return res.status(200).json(message);
-    else return res.status(400).json(message);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      errCode: -1,
-      message: "Error form the server!!!",
-    });
-  }
-};
-
-let handleUpdateProductType = async (req, res) => {
-  try {
-    let message = await productTypeService.updateProductTypeService(req.body);
-    if (message.errCode === 0) return res.status(200).json(message);
-    else return res.status(400).json(message);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      errCode: -1,
-      message: "Error form the server!!!",
-    });
-  }
-};
-
 let handleGetAllProductType = async (req, res) => {
   try {
     let { limit, page, sort, name, pagination } = req.query;
@@ -67,10 +22,7 @@ let handleGetAllProductType = async (req, res) => {
 };
 
 module.exports = {
-  handleCreateNewProductType,
-  handleDeleteProductType,
-  handleUpdateProductType,
-  handleGetAllProductType,
+  handleGetAllProductType
 };
 
 // so trigger curron tran

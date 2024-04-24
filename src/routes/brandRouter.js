79668568be7e12edcbbd2e41799b99cb8a465064
thuandevnguyen <thuandnk203@gmail.com>
2024-04-24 21:21:47 +0700

    fix bug

--- src/routes/brandRouter.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import brandController from "../controllers/brandController";
-let router = express.Router();
-
-router.get("/get-all-brand", brandController.handleGetAllBrand);
-export default router;
